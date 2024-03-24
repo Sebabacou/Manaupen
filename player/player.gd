c92389dee17fc1c6268e52ad33abd7cc563d82e4
ronan_botrel <ronan.botrel@epietch.eu>
2024-03-24 17:19:15 +0100

    adding remove ball when is touch wall and adding the output of the ball on the gun

--- player/player.gd
+++ player/player.gd
@@ -16,32 +16,32 @@ var health = 100
 var alive = true
 
 func _physics_process(delta):
-	var direction_right_left = Input.get_axis("ui_left", "ui_right")
-	var direction_down_up = Input.get_axis("ui_up", "ui_down")
+	var direction_right_left = Input.get_axis("move_left", "move_right")
+	var direction_down_up = Input.get_axis("move_up", "move_down")
 	shoot_timer += delta
 
-	if Input.get_axis("ui_right", "ui_left"):
+	if Input.get_axis("move_right", "move_left"):
 		velocity.x = direction_right_left * SPEED
 		animation.scale.x = 2 
-		if Input.is_action_pressed("ui_left"):
+		if Input.is_action_pressed("move_left"):
 			animation.scale.x = -2
 	else:
 		animation.scale.x *= 1
 		velocity.x = move_toward(velocity.x, 0, 30)
 		
-	if Input.get_axis("ui_up", "ui_down"):
+	if Input.get_axis("move_up", "move_down"):
 		velocity.y = direction_down_up * SPEED
 	else:
 		animation.scale.y *= 1
 		velocity.y = move_toward(velocity.y, 0, 30)
 		
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("move_left"):
 		animation.play("run")
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("move_right"):
 		animation.play("run")
-	elif Input.is_action_pressed("ui_up"):
+	elif Input.is_action_pressed("move_up"):
 		animation.play("run")
-	elif Input.is_action_pressed("ui_down"):
+	elif Input.is_action_pressed("move_down"):
 		animation.play("run")
 	else:
 		animation.play("idle")
@@ -56,5 +56,9 @@ func _physics_process(delta):
 
 func shoot():
 	var Bullet = BulletPath.instantiate()
+	Bullet.position = get_global_position() + (Vector2.from_angle(rotation) * 15)
+	if animation.scale.x == -2:
+			Bullet.position = get_global_position() + (Vector2.from_angle(rotation) * -15)
 	get_parent().add_child(Bullet)
-	Bullet.position = $Node2D/Marker2D.global_position
+
+const BALL_GROUP = "ball"
